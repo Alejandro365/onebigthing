95a7ac8633de33cc8a8eac54d98cd6f3f75ba558
cmejia <checha365@gmail.com>
2019-01-24 13:56:12 -0600

    Ejercicio hecho en casa

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get ('/hola', function(req, res, next) {
-  res.json({"mensaje":"hola"});
+//Ruta Hola
+router.get('/hola', function(req, res, next) {
+  res.render('hola',{});
 });
 
+//Ruta Expediente
+router.get('/expediente', function(req, res, next) {
+  res.render('expediente',{});
+});
+
+//Ruta Adios 
+router.get('/adios', function(req, res, next) {
+  res.json({"mensaje":"adios"});
+});
+ 
 module.exports = router;
 
-// var indexRoutes = require('routes/index');
